5f4eb7fb2b0d2bc9e4514fc0b7b5d8df39b68f7e
carolardilae <142743220+carolardilae@users.noreply.github.com>
2024-04-29 18:22:37 -0500

    Add files via upload

---
+++ pa/primero.py
@@ -0,0 +1,11 @@
+def pedir_numero (knumero):
+    knumero = input("Por favor, ingresa un número: ")
+    print (knumero)
+
+    #x1= 45
+    #x2= 1
+    #knumero = input("Por favor, ingresa un número: ")
+    #if x1 == knumero*x2:
+    #    print("xd")
+
+#en el print se puede poner cualquier numero pues porque no importa que valor se le asigne a n 
